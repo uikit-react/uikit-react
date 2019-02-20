fe7bf5963d5d2af89915b2ee11b1c7baf1e53121
Tony Gorez <tonygorez@gmail.com>
2019-02-20 23:08:13 +0100

    finished section component

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,51 +3,49 @@ import PropTypes from 'prop-types'
 import classnames from 'classnames'
 
 const Section = ({
-    children,
-    modifiers,
-    preserveColor,
-    sizeModifier,
-    overlap
+  children,
+  styleModifiers,
+  preserveColor,
+  sizeModifier,
+  overlap
 }) => {
+  const classes = classnames({
+    'uk-section': true,
+    'uk-preserve-color': preserveColor,
+    'uk-section-overlap': overlap,
+    [`uk-section-${styleModifiers}`]: styleModifiers,
+    [`uk-section-${sizeModifier}`]: sizeModifier
+  })
 
-    const classes = classnames({
-        'uk-section': true,
-        'uk-preserve-color': preserveColor,
-        'uk-section-overlap': overlap,
-        [`uk-section-${modifiers}`]: modifiers,
-        [`uk-section-${sizeModifier}`]: sizeModifier
-    })
-    
-    return (
-        <div className={classes}>
-            <div className="uk-container">
-                {children}
-            </div>
-        </div>
-    )
-
+  return (
+    <div className={classes}>
+      <div className='uk-container'>
+        {children}
+      </div>
+    </div>
+  )
 }
 
 Section.propTypes = {
-    children: PropTypes.oneOfType([
-        PropTypes.arrayOf(PropTypes.node),
-        PropTypes.node
-    ]),
-    modifiers: PropTypes.oneOf([
-        'default',
-        'muted',
-        'primary',
-        'secondary'
-    ]),
-    preserveColor: PropTypes.bool.isRequired,
-    overlap: PropTypes.bool.isRequired,
-    sizeModifier: PropTypes.oneOf([
-        'xsmall',
-        'small',
-        'large',
-        'xlarge',
-        'remove-vertical'
-    ])
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]),
+  styleModifiers: PropTypes.oneOf([
+    'default',
+    'muted',
+    'primary',
+    'secondary'
+  ]),
+  preserveColor: PropTypes.bool.isRequired,
+  overlap: PropTypes.bool.isRequired,
+  sizeModifier: PropTypes.oneOf([
+    'xsmall',
+    'small',
+    'large',
+    'xlarge',
+    'remove-vertical'
+  ])
 }
 
 export default Section
