dc4eefdd990806f2786205f0fbdd35c183a5b397
Tony Gorez <gorez.tony@gmail.com>
2019-02-27 19:32:59 +0100

    add component and story (#43)

---
+++ src/components/Section.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+const Section = ({
+  children,
+  styleModifiers,
+  preserveColor,
+  sizeModifier,
+  overlap
+}) => {
+  const classes = classnames({
+    'uk-section': true,
+    'uk-preserve-color': preserveColor,
+    'uk-section-overlap': overlap,
+    [`uk-section-${styleModifiers}`]: styleModifiers,
+    [`uk-section-${sizeModifier}`]: sizeModifier
+  })
+
+  return (
+    <div className={classes}>
+      <div className='uk-container'>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+Section.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]),
+  styleModifiers: PropTypes.oneOf([
+    'default',
+    'muted',
+    'primary',
+    'secondary'
+  ]),
+  preserveColor: PropTypes.bool.isRequired,
+  overlap: PropTypes.bool.isRequired,
+  sizeModifier: PropTypes.oneOf([
+    'xsmall',
+    'small',
+    'large',
+    'xlarge',
+    'remove-vertical'
+  ])
+}
+
+export default Section
